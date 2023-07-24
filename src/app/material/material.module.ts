750923276d431ec33b3b24395c18d84c78593471
Rocket <PhonlapatS.2002@gmail.com>
2023-07-24 23:22:18 +0700

    first commit

---
+++ src/app/material/material.module.ts
@@ -0,0 +1,22 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import {Component} from '@angular/core';
+import {MatIconModule} from '@angular/material/icon';
+import {MatDividerModule} from '@angular/material/divider';
+import {MatButtonModule} from '@angular/material/button';
+
+
+@NgModule({
+  declarations: [],
+  imports: [
+    CommonModule
+  ],
+  exports: [
+    MatButtonModule,
+    MatDividerModule,
+    MatIconModule,
+
+  ],
+})
+export class MaterialModule { }
+
